988d688cef96652782ae255edbd8181e4d54811f
lyanez22 <litzy.yanez76@myhunter.cuny.edu>
2025-10-01 21:51:51 -0400

    mexsu_internship_litzy_yanez

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className= "App">
-      <h1>Hello, MEXSU!</h1>
+      <h1 className="fade-in">
+        Hello, {" "}
+        <span className="animated">M</span>
+        <span className="animated">E</span>
+        <span className="animated">X</span>
+        <span className="animated">S</span>
+        <span className="animated">U</span>!
+        </h1>
   </div>
   );
 }
